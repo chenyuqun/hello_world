6d9971907fbf6aaa81f92322f6935589d4c87bf4
chenyuqun <346573209@qq.com>
2016-06-16 10:08:36 +0800

    文件读取

---
+++ src/zizaike/IsSpeedRoom.java
@@ -0,0 +1,20 @@
+package zizaike;
+
+/**
+ * Project Name: github <br/>
+ * Function:IsSpeedRoom. <br/>
+ * date: 2016/6/6 16:46 <br/>
+ *
+ * @author alex
+ * @since JDK 1.7
+ */
+public class IsSpeedRoom {
+    public static void main(String args[]){
+        String rawData="";
+        String[] ridList=rawData.split(",");
+        for(String rid:ridList){
+            System.out.println(rid);
+        }
+
+    }
+}
